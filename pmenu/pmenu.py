67c5b87b20538999f5f554665befb775d5614482
Radi Mustafa <rmustafa@cocus.com>
2020-08-10 15:44:03 +0200

    refactor: update CLI description

--- pmenu/pmenu.py
+++ pmenu/pmenu.py
@@ -10,9 +10,9 @@ if __name__ == '__main__':
     shell_program | xargs pmenu
     """
     parser = argparse.ArgumentParser(description='pmenu')
-    parser.add_argument('-H', '--app_height', type=int, help='App height')
-    parser.add_argument('-i', '--list_item_height', type=int, help='List item height')
-    parser.add_argument('-n', '--list_max_items', type=int, help='Total number of view items')
+    parser.add_argument('-H', '--app_height', type=int, help='Total height of the application')
+    parser.add_argument('-i', '--list_item_height', type=int, help='Item height')
+    parser.add_argument('-n', '--list_max_items', type=int, help='Total number of items to be viewed in the list')
     parser.add_argument('-w', '--input_width', type=int, help='Width of search field')
     parser.add_argument('-l', '--list', action='store_true', help='Show vertically as list')
     group = parser.add_mutually_exclusive_group()
