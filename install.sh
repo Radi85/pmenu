5a9295d7f8a77ade9e57a8f29e0d9dafe149b6e2
Radi Mustafa <rmustafa@cocus.com>
2020-08-11 11:29:22 +0200

    fix: update install script

--- install.sh
+++ install.sh
@@ -3,8 +3,7 @@
 python3.6 -m venv pmenu_venv;
 source pmenu_venv/bin/activate;
 pip install --upgrade pip && pip install -r requirements.txt;
-PyInstaller pmenu/pmenu.py --onefile;
+python3.6 -m PyInstaller pmenu/pmenu.py --onefile;
 cp pmenu/.pmenu.ini $HOME/.pmenu.ini;
-rm -rf pmenu_venv
 sudo cp dist/pmenu /usr/local/bin/pmenu;
 pmenu "welcome to" pmenu app press escape "to exit"
